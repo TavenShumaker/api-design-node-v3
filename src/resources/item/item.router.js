d662aedc3cdecbe70b1292d86d6dbf6f416ddc55
Taven Shumaker <tavensshumaker@gmail.com>
2021-02-17 00:55:39 -0800

    completed lesson-2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,37 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const router = Router()
+const router = Router();
 
-export default router
+// create a router for the Item resource
+// create full crud routes and create placeholder controllers
+router
+  .route('/')
+  .get((req, res) => {
+    res.status(200).send({ message: 'Response from get request.' });
+  })
+  .post((req, res) => {
+    console.log('Post Request:', req.body);
+    res.status(200).send({ message: 'Response from post request.' });
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res
+      .status(200)
+      .send({ message: `Response from get request with ID ${req.params.id}.` });
+  })
+  .put((req, res) => {
+    console.log('Put Request:', req.body);
+    res
+      .status(200)
+      .send({ message: `Response from put request with ID ${req.params}.` });
+  })
+  .delete((req, res) => {
+    console.log('Delete Request:', req.body);
+    res
+      .status(200)
+      .send({ message: `Response from delete request with ID ${req.params}.` });
+  });
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,26 +1,36 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import config from './config'
-import cors from 'cors'
-import { connect } from './utils/db'
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import config from './config';
+import cors from 'cors';
+import { connect } from './utils/db';
+import itemRouter from './resources/item/item.router';
 
-export const app = express()
+// This exercise will have you creating routes and sub routers for our soon the be DB resources using Express routing and routers
 
-app.disable('x-powered-by')
+// mount router on the root server
+// ensure all tests pass by running test command
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+export const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+// allows for the use of url params
+app.use(urlencoded({ extended: true }));
+// logs metrics related to http
+app.use(morgan('dev'));
+
+app.use('/api/item', itemRouter);
 
 export const start = async () => {
   try {
-    await connect()
+    await connect();
     app.listen(config.port, () => {
-      console.log(`REST API on http://localhost:${config.port}/api`)
-    })
+      console.log(`REST API on http://localhost:${config.port}/api`);
+    });
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-}
+};
